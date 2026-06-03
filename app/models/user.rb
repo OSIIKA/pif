e00b24127263c98550617763bff817a6ea55e30c
OSIIKA <shun20080204@gmail.com>
2026-06-03 21:09:03 +0900

    user.rbPasswordCheck

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
+  # bcryptとpassword_digestを連動させる魔法の1行
+  has_secure_password
+  # 既存のリレーション（そのまま残します）
   belongs_to :user_lank, foreign_key: :level, optional: true
+  # セキュリティシステムとバリデーションの追加
+  # ユーザー名のチェック
+  validates :name, presence: true, uniqueness: true
+  # メールアドレスのチェック
+  validates :mail, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  # パスワードのチェック
+  validates :password, length: { minimum: 6 }, if: -> { new_record
 end
