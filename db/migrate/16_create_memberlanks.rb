486b21228e442a36a55fc9637d587e491d6cda37
OSIIKA <shun20080204@gmail.com>
2026-06-05 08:19:12 +0900

    memberlanks

---
+++ db/migrate/16_create_memberlanks.rb
@@ -0,0 +1,9 @@
+class CreateMemberlanks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :user_lanks do |t|
+      t.string :name, null: false# ランク名
+      t.text :text# ランク説明
+      t.timestamps
+    end
+  end
+end
