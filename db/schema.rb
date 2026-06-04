ff4a91ff685a7f75e72c16d10d5ead4268c1e584
OSIIKA <shun20080204@gmail.com>
2026-06-04 15:26:46 +0900

    schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_03_22_020509) do
+ActiveRecord::Schema.define(version: 16) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "battleunits", force: :cascade do |t|
-    t.string "name"
-    t.integer "hp"
-    t.integer "atk"
-    t.string "info"
-  end
-
-  create_table "enemyfreets", force: :cascade do |t|
+  create_table "allfreets", force: :cascade do |t|
     t.integer "stage"
     t.string "name"
     t.integer "hp"
     t.integer "max_hp"
+    t.integer "atk"
+    t.string "info"
+  end
+
+  create_table "battleunits", force: :cascade do |t|
+    t.string "name"
+    t.integer "hp"
     t.integer "atk"
     t.string "info"
   end
@@ -48,6 +48,16 @@ ActiveRecord::Schema.define(version: 2025_03_22_020509) do
     t.integer "battle", default: 0
   end
 
+  create_table "user_lanks", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "text"
+    t.integer "event1", null: false
+    t.integer "event2", null: false
+    t.integer "required_exp", default: 0
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "user_myfreets", force: :cascade do |t|
     t.integer "user_id"
     t.integer "myfreet_id"
@@ -58,9 +68,12 @@ ActiveRecord::Schema.define(version: 2025_03_22_020509) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "mail"
-    t.string "password_digest", null: false
+    t.string "password_digest"
     t.integer "level"
     t.integer "exp"
+    t.string "provider"
+    t.string "uid"
+    t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
   end
 
 end
