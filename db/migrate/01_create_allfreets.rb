4db5bbe5cd501b11966e3f0b2078c2ad4d59f4b0
OSIIKA <shun20080204@gmail.com>
2026-06-09 18:07:37 +0900

    gacha6

--- db/migrate/01_create_allfreets.rb
+++ db/migrate/01_create_allfreets.rb
@@ -7,6 +7,14 @@ class CreateAllfreets < ActiveRecord::Migration[6.1]
       t.integer :max_hp
       t.integer :atk
       t.string :info
+
+      t.integer :rarity, default: 1
+
+      t.integer :normal, default: 0
+      t.integer :rare, default: 0
+      t.integer :limited, default: 0
+      t.integer :story, default: 0
+      t.integer :event, default: 0
     end
   end
 end
