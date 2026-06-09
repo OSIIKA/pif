e29b275eebd877cdc31249566a1257554efce7fe
OSIIKA <shun20080204@gmail.com>
2026-06-09 18:19:45 +0900

    gacha7

--- app/models/allfreet.rb
+++ app/models/allfreet.rb
@@ -1,2 +1,11 @@
 class Allfreet < ActiveRecord::Base
+  # 🟢 数字をレア度の文字列に変換する翻訳機
+  def rarity_text
+    case rarity
+    when 1 then "N"
+    when 2 then "R"
+    when 3 then "SR"
+    else "???" # 万が一想定外の数字が入ったときの保険
+    end
+  end
 end
