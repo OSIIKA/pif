68a2d012a23b0aaba1d067cebb7f8e1e376f9282
OSIIKA <shun20080204@gmail.com>
2026-06-13 22:25:25 +0900

    gacha58

--- db/migrate/17_create_usersteps.rb
+++ db/migrate/17_create_usersteps.rb
@@ -1,12 +1,12 @@
 class CreateUsersteps < ActiveRecord::Migration[6.1]
   def change
-    create_table :user_steps do |t|
+    create_table :usersteps do |t|
       t.integer :user_id, null: false # どのユーザーのステップか
       t.integer :limited_gacha_step, :integer, default: 1 # 現在のステップ（例: 1, 2, 3 ...）
 
     end
 
     # よく検索する項目にはインデックスを貼ると爆速になります
-    add_index :user_steps, :user_id
+    add_index :usersteps, :user_id
   end
 end
