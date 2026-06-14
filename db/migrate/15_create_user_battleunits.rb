3ba43a3ad8c43349704cf90b64c1d64d01e5d250
OSIIKA <shun20080204@gmail.com>
2026-06-14 13:58:49 +0900

    base9

---
+++ db/migrate/15_create_user_battleunits.rb
@@ -0,0 +1,24 @@
+class CreateUserBattleunits < ActiveRecord::Migration[6.1]
+  def change
+    create_table :user_battleunits do |t|
+      t.integer :user_id, null: false        # 👤 どのユーザーの艦隊か
+      t.integer :fleet_number, null: false   # 🔢 何番目の艦隊か（1 = 第一艦隊、6 = 第六艦隊）
+      
+      # 🚩 旗艦枠（1隻）
+      t.integer :flagship_id                 # 所持している艦船のユニークID
+      
+      # ⚓ 随伴艦枠（6隻）
+      t.integer :sub_ship_1_id
+      t.integer :sub_ship_2_id
+      t.integer :sub_ship_3_id
+      t.integer :sub_ship_4_id
+      t.integer :sub_ship_5_id
+      t.integer :sub_ship_6_id
+
+      t.timestamps
+    end
+    
+    # 「ユーザーID」と「艦隊番号」の組み合わせにインデックスを張る（検索爆速化＆重複防止）
+    add_index :user_fleets, [:user_id, :fleet_number], unique: true
+  end
+end
