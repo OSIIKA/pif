0cb5d22d68056612c2074c58d2d90d7284890385
OSIIKA <shun20080204@gmail.com>
2026-06-15 20:08:58 +0900

    battle45

--- controllers/battle_controller.rb
+++ controllers/battle_controller.rb
@@ -65,6 +65,10 @@ get '/battle/set' do
 
   # 📦 画面に必要な家具を、リロードに強いセッションから安全に復元するだけ！
   @stage = session[:battle_stage] || 1
+  
+  # 古い味方配置を一旦クリアする
+  session[:battle_allies] = nil if params[:phase].blank? && !session[:battle_enemies].blank?
+
   @fleets = @user.user_battleunits.order(:fleet_number)
   
   # 🔍 もしストーリーから直接GETへ飛んできて敵データがまだ無ければ、ここで正しくロードする
@@ -186,73 +190,7 @@ post '/battle/start' do
   redirect '/battle/turn' #  /battle/turn へ
 end
 
-# ⚔️ B: 布陣確定後のデータ処理
-post '/battle/start' do
-  @user = User.find_by(id: session[:user])
-  redirect '/users/login' unless @user
 
-  battle_allies = []
-  (1..6).each do |fleet_num|
-    pos_str = params["fleet_#{fleet_num}_pos"]
-    next if pos_str.blank?
-
-    col, row = pos_str.split(',').map(&:to_i)
-    fleet_data = @user.user_battleunits.find_by(fleet_number: fleet_num)
-    next unless fleet_data
-
-    total_hp = 0
-    total_atk = 0
-    skill_logs = []
-
-    flagship = UserMyfreet.find_by(id: fleet_data.flagship_id)
-    sub_1    = UserMyfreet.find_by(id: fleet_data.sub_ship_1_id)
-    sub_2    = UserMyfreet.find_by(id: fleet_data.sub_ship_2_id)
-    sub_3    = UserMyfreet.find_by(id: fleet_data.sub_ship_3_id)
-    sub_4    = UserMyfreet.find_by(id: fleet_data.sub_ship_4_id)
-    sub_5    = UserMyfreet.find_by(id: fleet_data.sub_ship_5_id)
-    sub_6    = UserMyfreet.find_by(id: fleet_data.sub_ship_6_id)
-
-    ships = [{ ship: flagship, is_flag: true }, sub_1, sub_2, sub_3, sub_4, sub_5, sub_6].compact
-
-    ships.each do |item|
-      ship = item.is_a?(Hash) ? item[:ship] : item
-      next unless ship
-
-      base_hp = ship.allfreet.hp
-      base_atk = ship.allfreet.atk
-
-      if fleet_num == 1 && item.is_a?(Hash) && item[:is_flag]
-        base_hp *= 10
-        base_atk *= 10
-        skill_logs << "第一艦隊旗艦戦技：【臨界突破・十倍界王拳】発動！"
-      end
-
-      total_hp += base_hp
-      total_atk += base_atk
-    end
-
-    battle_allies << {
-      fleet_number: fleet_num,
-      name: "第#{fleet_num}艦隊",
-      max_hp: total_hp,
-      hp: total_hp,
-      atk: total_atk,
-      col: col,
-      row: row,
-      skills: skill_logs
-    }
-  end
-
-  session[:battle_allies] = battle_allies
-
-  
-
-  session[:battle_logs] = []
-
-  # ❌ 修正前: redirect '/battle/prepare'
-  # ⭕ 修正後: 別ページにいかず、元の画面に「演出モードの鍵」を持たせて戻る！
-  redirect '/battle?phase=prepare'
-end
 
 #バトル画面で自分のキャラと相手のキャラを選択して攻撃を実行する際に行う処理
 post '/battle/attack' do
