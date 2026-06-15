c1fd6f213f297162c1e27234bea3cc4f12b49c76
OSIIKA <shun20080204@gmail.com>
2026-06-15 23:57:23 +0900

    event4

--- controllers/home_controller.rb
+++ controllers/home_controller.rb
@@ -11,6 +11,8 @@ get '/home' do
     # 💡 [追記] 全体チャットの最新30件を、古い順（時系列順）で取得して画面に渡す
     # 最新の30件を降順で取ってから、画面表示のために reverse で昇順に戻しています
     @global_chats = Chat.where(category: 'global').order(created_at: :desc).limit(30).reverse
+    @personal_events = PERSONAL_EVENT_SCHEDULES
+    @alliance_events = ALLIANCE_EVENT_SCHEDULES
   end
     
   erb :home
