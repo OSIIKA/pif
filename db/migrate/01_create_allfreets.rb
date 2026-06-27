a206cb3710dfa38c83017110ac4e2aea86e055f0
OSIIKA <shun20080204@gmail.com>
2026-06-27 18:43:03 +0900

    CHECK9

--- db/migrate/01_create_allfreets.rb
+++ db/migrate/01_create_allfreets.rb
@@ -1,7 +1,13 @@
+# 01_create_allfreets.rb
+# 0?_から始まるマイグレーションファイルは、データベースの初期設定を行うためのものです。
+# これにはUserなどと接続する外部キーは付属せず、必ず最初に実行し、辞書目的でのみ使用されます。
 class CreateAllfreets < ActiveRecord::Migration[6.1]
   def change
     create_table :allfreets do |t|
+      # 敵艦を検索するために暫定的に置いているカラム
+      # 必要に応じて敵艦中間テーブルに移動する
       t.integer :stage
+      # どの艦艇も持っている基本情報
       t.string :name
       t.integer :hp
       t.integer :max_hp
--- db/migrate/02_create_stories.rb
+++ db/migrate/02_create_stories.rb
@@ -1,3 +1,6 @@
+# 02_create_stories.rb
+# 0?_から始まるマイグレーションファイルは、データベースの初期設定を行うためのものです。
+# これにはUserなどと接続する外部キーは付属せず、必ず最初に実行し、辞書目的でのみ使用されます。
 class CreateStories < ActiveRecord::Migration[6.1]
   def change
     create_table :stories do |t|
