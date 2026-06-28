dea1b8fbd735f2a9bf3104adeca5623f50812155
OSIIKA <shun20080204@gmail.com>
2026-06-29 00:51:01 +0900

    CHECK44

--- controllers/shop_controller.rb
+++ controllers/shop_controller.rb
@@ -6,7 +6,10 @@ get '/shop' do
   # 💡 ログインしているユーザーのデータを取得（セッション管理の仕様に合わせて調整してください）
   @user = User.find_by(id: session[:user])
   redirect '/users/new' if @user.nil?
-
+  # 🔩 紫鉄の所持数を取得
+  iron_item = Item.find_by(type: 0, rarity: 1)
+  user_iron = @user.user_items.find_by(item_id: iron_item.id)
+  @iron_count = user_iron ? user_iron.count : 0
   # 🛒 ショップの商品一覧を取得（Weaponモデルから全ての武器を取得）
   @shop_items = Weapon.all.order(:rarity, :id) # レアリティ順、ID順でソート
 
