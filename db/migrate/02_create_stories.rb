3b8eb62ae59623c2e6074b2a0b5ae67f4d9aa293
OSIIKA <shun20080204@gmail.com>
2026-06-29 21:48:57 +0900

    CHECK51

--- db/migrate/02_create_stories.rb
+++ db/migrate/02_create_stories.rb
@@ -1,6 +1,7 @@
 # 02_create_stories.rb（モデル確認済み）
 # 0?_から始まるマイグレーションファイルは、データベースの初期設定を行うためのものです。
 # これにはUserなどと接続する外部キーは付属せず、必ず最初に実行し、辞書目的でのみ使用されます。
+# ストーリーを原作通りに記述するため、共通規格とは異なるシード方式を採用しています。詳しくは db/seeds.rb を参照してください。
 class CreateStories < ActiveRecord::Migration[6.1]
   def change
     create_table :stories do |t|
